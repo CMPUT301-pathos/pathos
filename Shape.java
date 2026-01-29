68e53ce0af460895b401bc9ba9a03e2b06548d2b
Kenneth Joseph <114956555+KennethJ1@users.noreply.github.com>
2026-01-28 17:56:34 -0700

    lol

--- Shape.java
+++ Shape.java
@@ -1,5 +1,15 @@
 public abstract class Shape {
-    protected in x;
-    protected int y
+    protected int x;
+    protected int y;
 
+    public abstract class Shape {
+        protected int y;
+        protected int x;
+        String color = "#FFFF";
+
+        public Shape(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
 }
