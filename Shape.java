e9a63ce46633be2dd4023db93be067bde95d2fbe
Dmitriy Limanets <156034288+DimaLimanets@users.noreply.github.com>
2026-01-28 18:47:59 -0700

    Refactor Shape class by removing inner class
    Removed redundant inner class definition and adjusted indentation.

--- Shape.java
+++ Shape.java
@@ -1,15 +1,10 @@
 public abstract class Shape {
     protected int x;
     protected int y;
-
-    public abstract class Shape {
-        protected int y;
-        protected int x;
-        String color = "#FFFF";
+    String color = "#FFFF";
 
         public Shape(int x, int y) {
             this.x = x;
             this.y = y;
         }
-    }
 }
