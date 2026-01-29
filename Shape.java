2c2079c5495076210c37ae7251713089956a0b71
Dmitriy Limanets <156034288+DimaLimanets@users.noreply.github.com>
2026-01-28 18:41:06 -0700

    Refactor Shape class structure and remove redundancy

--- Shape.java
+++ Shape.java
@@ -2,14 +2,10 @@ public abstract class Shape {
     protected int x;
     protected int y;
 
-    public abstract class Shape {
-        protected int y;
-        protected int x;
-        String color = "#FFFF";
+    String color = "#FFFF";
 
         public Shape(int x, int y) {
             this.y = y;
             this.x = x;
         }
-    }
 }
