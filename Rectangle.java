8dd476be8ab4fc32e1a3977b07ad4c2373c1d479
Dima Limanets <limanets@ualberta.ca>
2026-01-30 15:57:00 -0700

    Final

--- Rectangle.java
+++ Rectangle.java
@@ -1,7 +1,8 @@
 public class Rectangle extends Shape {
     int width, height;
 
-    public Rectangle(int width, int height){
+    public Rectangle(int x, int y, int width, int height){
+        super(x,y);
         this.width = width;
         this.height = height;
     }
