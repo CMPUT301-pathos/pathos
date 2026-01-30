2a6e2b5f285671115b769ca673439959899e2c48
edwinshaijudavid <edwinshaiju.david@gmail.com>
2026-01-30 13:11:05 -0700

    adding triangle shape

---
+++ Triangle.java
@@ -0,0 +1,10 @@
+public class Triangle extends Shape {
+    int width;
+    int height;
+
+    public Triangle(int x, int y, int width, int height) {
+        super(x, y);
+        this.width = width;
+        this.height = height;
+    }
+}
